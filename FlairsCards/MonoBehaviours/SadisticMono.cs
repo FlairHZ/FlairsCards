97a44e6dfacdfdbe97d82a5a421af16d0f4caaac
aidenwvt <aidenwiehnschool@gmail.com>
2024-08-06 20:29:57 -0400

    dang

--- FlairsCards/MonoBehaviours/SadisticMono.cs
+++ FlairsCards/MonoBehaviours/SadisticMono.cs
@@ -4,72 +4,27 @@ using System.Collections;
 using ModdingUtils.MonoBehaviours;
 using WillsWackyManagers.Utils;
 using FC.Extensions;
-using ModdingUtils.GameModes;
-using static ModdingUtils.Utils.SortingController;
-using ModdingUtils.Extensions;
-
-/*namespace FlairsCards.MonoBehaviours
-{
-    class SadisticMono : ReversibleEffect, IRoundStartHookHandler, IRoundEndHookHandler, IGameStartHookHandler
-    {
-        public override void OnStart()
-        {
-            InterfaceGameModeHooksManager.instance.RegisterHooks(this);
-            applyImmediately = false;
-        }
-
-        public override void OnOnDestroy()
-        {
-            InterfaceGameModeHooksManager.instance.RemoveHooks(this);
-        }
-
-        public void OnRoundStart()
-        {
-            this.ClearModifiers();
-            UpdateMultipliers();
-            this.ApplyModifiers();
-        }
-        public void OnRoundEnd()
-        {
-            this.ClearModifiers();
-        }
-
-        public void OnGameStart()
-        {
-            UnityEngine.GameObject.Destroy(this);
-        }
-
-        private void UpdateMultipliers()
-        {
-            this.gunStatModifier.damage_add = (float)(player.data.stats.GetAdditionalData().curses * 0.2);
-            this.characterStatModifiersModifier.movementSpeed_add = (float)(player.data.stats.GetAdditionalData().curses * 0.2);
-        }
-    }
-}*/
 
 namespace FlairsCards.MonoBehaviours
 {
-    class SadisticMono : ReversibleEffect
+    class SadisticMono : MonoBehaviour
     {
-        public override void OnStart()
+        private Player player;
+        private void Start()
         {
             player = GetComponentInParent<Player>();
             GameModeManager.AddHook(GameModeHooks.HookPickEnd, PickEnd);
         }
 
-        public override void OnOnDestroy()
+        private void OnDestroy()
         {
             GameModeManager.RemoveHook(GameModeHooks.HookPickEnd, PickEnd);
         }
 
         IEnumerator PickEnd(IGameModeHandler gm)
         {
-            /*ClearModifiers();
-            gunStatModifier.damage_add = (float)(player.data.stats.GetAdditionalData().curses * 0.2);
-            characterStatModifiersModifier.movementSpeed_add = (float)(player.data.stats.GetAdditionalData().curses * 0.2);
-            ApplyModifiers();*/
-            player.data.stats.movementSpeed += (player.data.stats.GetAdditionalData().curses);
-            
+            player.data.stats.movementSpeed = (float)(1.0 + (player.data.stats.GetAdditionalData().curses * 0.2));
+
             yield break;
         }
     }

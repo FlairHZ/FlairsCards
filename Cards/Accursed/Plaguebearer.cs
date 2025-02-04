a78d3c1299a1226a72758e3a981685ea3b20f7fe
Flair <142646612+FlairHZ@users.noreply.github.com>
2025-02-03 22:27:29 -0500

    v0.1.5 Gambler and Speedster UI changes
    Some Gambler and Speedster UI changes for clarity, minor code rewrites, bugfixes

---
+++ Cards/Accursed/Plaguebearer.cs
@@ -0,0 +1,74 @@
+﻿using ClassesManagerReborn.Util;
+using FC.Extensions;
+using FlairsCards.MonoBehaviours;
+using FlairsCards.Utilities;
+using RarityLib.Utils;
+using UnboundLib;
+using UnboundLib.Cards;
+using UnityEngine;
+
+namespace FlairsCards.Cards
+{
+    class Plaguebearer : CustomCard
+    {
+        internal static CardInfo Card = null;
+        public override void Callback()
+        {
+            gameObject.GetOrAddComponent<ClassNameMono>().className = AccursedClass.name;
+        }
+        public override void SetupCard(CardInfo cardInfo, Gun gun, ApplyCardStats cardStats, CharacterStatModifiers statModifiers, Block block)
+        {
+            cardInfo.allowMultiple = false;
+            statModifiers.health = 1.25f;
+            FCDebug.Log($"[{FlairsCards.ModInitials}][Card] {GetTitle()} has been setup.");
+        }
+        public override void OnAddCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats)
+        {
+            player.gameObject.GetOrAddComponent<PlaguebearerMono>();
+            FCDebug.Log($"[{FlairsCards.ModInitials}][Card] {GetTitle()} has been added to player {player.playerID}.");
+        }
+        public override void OnRemoveCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats)
+        {
+            Destroy(player.gameObject.GetOrAddComponent<PlaguebearerMono>());
+            FCDebug.Log($"[{FlairsCards.ModInitials}][Card] {GetTitle()} has been removed to player {player.playerID}.");
+        }
+        protected override string GetTitle()
+        {
+            return "Plaguebearer";
+        }
+        protected override string GetDescription()
+        {
+            return "Overwhelm with sickness, every player gets a curse each turn";
+        }
+        protected override GameObject GetCardArt()
+        {
+            return null;
+            //return FlairsCards.CardArtPlaguebearer;
+        }
+        protected override CardInfo.Rarity GetRarity()
+        {
+            return RarityUtils.GetRarity("CommonClass");
+        }
+        protected override CardInfoStat[] GetStats()
+        {
+            return new CardInfoStat[]
+            {
+                new CardInfoStat()
+                {
+                    positive = true,
+                    stat = "Health",
+                    amount = "+25%",
+                    simepleAmount = CardInfoStat.SimpleAmount.Some
+                },
+            };
+        }
+        protected override CardThemeColor.CardThemeColorType GetTheme()
+        {
+            return CardThemeColor.CardThemeColorType.DestructiveRed;
+        }
+        public override string GetModName()
+        {
+            return FlairsCards.ModInitials;
+        }
+    }
+}

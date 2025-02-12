9c90cb386da3fb41e9a7ab519b2b25a4a70fe75f
Flair <142646612+FlairHZ@users.noreply.github.com>
2025-02-12 14:50:48 -0500

    v0.2.0 Minor card reworks

---
+++ FlairsCards/FlairsCards/Monobehaviours/PlaguebearerMono.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Linq;
+using UnboundLib.GameModes;
+using UnityEngine;
+using WillsWackyManagers.Utils;
+
+namespace FlairsCards.MonoBehaviours
+{
+    class PlaguebearerMono : MonoBehaviour
+    {
+        private Player player;
+        private void Start()
+        {
+            player = GetComponentInParent<Player>();
+            GameModeManager.AddHook(GameModeHooks.HookPickEnd, PickEnd);
+        }
+
+        private void OnDestroy()
+        {
+            GameModeManager.RemoveHook(GameModeHooks.HookPickEnd, PickEnd);
+        }
+
+        IEnumerator PickEnd(IGameModeHandler gm)
+        {
+            for (int i = 0; i < PlayerManager.instance.players.Count; i++)
+            {
+                var chosenPlayer = PlayerManager.instance.players[i];
+                CurseManager.instance.CursePlayer(chosenPlayer, (curse) => { ModdingUtils.Utils.CardBarUtils.instance.ShowImmediate(chosenPlayer, curse); });
+            }
+
+            yield break;
+        }
+    }
+}

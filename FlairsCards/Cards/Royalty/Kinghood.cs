ae9401ee0ed7f27e2554d1bda0650f95e95698dd
Flair <142646612+FlairHZ@users.noreply.github.com>
2025-02-12 14:52:04 -0500

    v0.2.0 Minor card reworks

---
+++ FlairsCards/Cards/Royalty/Kinghood.cs
@@ -0,0 +1,63 @@
+﻿using ClassesManagerReborn.Util;
+using FlairsCards.MonoBehaviours;
+using FlairsCards.Utilities;
+using RarityLib.Utils;
+using UnboundLib;
+using UnboundLib.Cards;
+using UnityEngine;
+
+namespace FlairsCards.Cards
+{
+    class Kinghood : CustomCard
+    {
+        internal static CardInfo Card = null;
+        public override void Callback()
+        {
+            gameObject.GetOrAddComponent<ClassNameMono>().className = RoyaltyClass.name;
+        }
+        public override void SetupCard(CardInfo cardInfo, Gun gun, ApplyCardStats cardStats, CharacterStatModifiers statModifiers, Block block)
+        {
+            cardInfo.allowMultiple = false;
+            FCDebug.Log($"[{FlairsCards.ModInitials}][Card] {GetTitle()} has been setup.");
+        }
+        public override void OnAddCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats)
+        {
+            player.gameObject.GetOrAddComponent<KinghoodMono>();
+            FCDebug.Log($"[{FlairsCards.ModInitials}][Card] {GetTitle()} has been added to player {player.playerID}.");
+        }
+        public override void OnRemoveCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats)
+        {
+            Destroy(player.gameObject.GetOrAddComponent<RoyaltyMono>());
+            FCDebug.Log($"[{FlairsCards.ModInitials}][Card] {GetTitle()} has been removed to player {player.playerID}.");
+        }
+
+        protected override string GetTitle()
+        {
+            return "Kinghood";
+        }
+        protected override string GetDescription()
+        {
+            return "Gain +100% damage after winning 3 rounds";
+        }
+        protected override GameObject GetCardArt()
+        {
+            return FlairsCards.CardArtKinghood;
+        }
+        protected override CardInfo.Rarity GetRarity()
+        {
+            return RarityUtils.GetRarity("RareClass");
+        }
+        protected override CardInfoStat[] GetStats()
+        {
+            return null;
+        }
+        protected override CardThemeColor.CardThemeColorType GetTheme()
+        {
+            return CardThemeColor.CardThemeColorType.FirepowerYellow;
+        }
+        public override string GetModName()
+        {
+            return FlairsCards.ModInitials;
+        }
+    }
+}
